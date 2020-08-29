0d015f38d6ac1905eed963d3dd55493d425f313a
Ayush Kulshrestha <akcool.ayush@gmail.com>
2020-08-29 18:23:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App appName="RiverSys"/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
