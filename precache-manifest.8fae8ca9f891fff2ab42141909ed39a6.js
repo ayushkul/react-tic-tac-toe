70b9ac4b1ca390ac1bc27b0f394e724bc2938f65
Ayush Kulshrestha <akcool.ayush@gmail.com>
2020-08-30 23:34:09 +0530

    Updates

---
+++ precache-manifest.8fae8ca9f891fff2ab42141909ed39a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff76896b8b0f848758277875f1f5eb72",
+    "url": "/react-demo-deploy/index.html"
+  },
+  {
+    "revision": "f9679ea44e8578a6bf17",
+    "url": "/react-demo-deploy/static/css/main.ba88e33b.chunk.css"
+  },
+  {
+    "revision": "856bd64a21dbd1a70913",
+    "url": "/react-demo-deploy/static/js/2.39a0290e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo-deploy/static/js/2.39a0290e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9679ea44e8578a6bf17",
+    "url": "/react-demo-deploy/static/js/main.1c004be8.chunk.js"
+  },
+  {
+    "revision": "a11e9a443b08e5ce8b1b",
+    "url": "/react-demo-deploy/static/js/runtime-main.cdab3cfd.js"
+  }
+]);
